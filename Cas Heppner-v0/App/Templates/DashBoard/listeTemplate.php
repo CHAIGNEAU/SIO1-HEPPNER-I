8a928743dd85ffabe9ad179e734e742b06497179
AdrienMiquel <Adrien.miquel0401@gmail.com>
2018-05-23 16:32:08 +0200

    d'ajout des conducteurs actuellement disponible

--- Cas Heppner-v0/App/Templates/DashBoard/listeTemplate.php
+++ Cas Heppner-v0/App/Templates/DashBoard/listeTemplate.php
@@ -48,13 +48,14 @@
             </div>
         </div>
     </div>
+
     <div class="col-xl-3 col-sm-6 mb-3">
         <div class="card text-white bg-primary o-hidden h-100">
             <div class="card-body">
                 <div class="card-body-icon">
                     <i class="fa fa-fw fa-globe"></i>
                 </div>
-                <div class="mr-5"><?php echo \App\M\ConducteurModel::getConducteursLibre(); ?> Conducteurs non affectés à un déplacement</div>
+                <div class="mr-5"><?php echo \App\M\TrajetModel::getConducteurNonLibre(); ?> Conducteurs actuellement en déplacement</div>
 
             </div>
         </div>
@@ -65,7 +66,7 @@
                 <div class="card-body-icon">
                     <i class="fa fa-fw fa-globe"></i>
                 </div>
-                <div class="mr-5"><?php echo \App\M\TrajetModel::getConducteurNonLibre(); ?> Conducteurs actuellement en déplacement</div>
+                <div class="mr-5"><?php echo \App\M\ConducteurModel::count() - \App\M\TrajetModel::getConducteurNonLibre(); ?> Conducteurs actuellement disponible !</div>
 
             </div>
         </div>
